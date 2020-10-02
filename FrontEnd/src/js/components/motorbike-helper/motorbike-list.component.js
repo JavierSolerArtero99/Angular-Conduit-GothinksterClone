1b4cf65a538188477f941f83020a15f53f3361b3
Javier Soler Artero <javier.soler.jsa@gmail.com>
2020-10-02 15:53:21 +0200

    FrontEnd: Filters DONE

--- FrontEnd/src/js/components/motorbike-helper/motorbike-list.component.js
+++ FrontEnd/src/js/components/motorbike-helper/motorbike-list.component.js
@@ -8,28 +8,6 @@ class MotorbikeListCtrl {
             console.log("Se ha llamado a openDetails() desde el CONTROLADOR DE LA LISTA")
             $state.go("app.motorbikeDetails", { id: this.motorbike["slug"] });
         };
-
-        //Filters
-        this.filtering = false;
-        this.cvFilter = "";
-    }
-
-    filter() {
-        this.filtering = true
-        this.noResults = false
-        let aux = this.motorbikes.filter((element) => element.cv === this.cvFilter);
-        this.motorbikes = aux
-        console.log("Filtered")
-        console.log(aux);
-
-        if (this.motorbikes.length < 1) {
-            this.noResults = true
-        }
-    }
-
-    cleanFilter() {
-        this.filtering = false;
-        this.motorbikes = cars;
     }
 }
 
--- FrontEnd/src/js/motorbike/motorbike.controller.js
+++ FrontEnd/src/js/motorbike/motorbike.controller.js
@@ -8,6 +8,30 @@ class MotorbikeCtrl {
         this._$scope = $scope;
         this._$scope.motorbikes = motorbikes;
         this.currentUser = User.current;
+        this.motorbikes = motorbikes;
+        this.auxMotorbikes = motorbikes;
+
+        // Filters
+        this.filtering = false;
+        this.cvFilter = "";
+        this.filter = function() {
+            this.filtering = true
+            this.noResults = false
+            let aux = this.motorbikes.filter((element) => element.cv === this.cvFilter);
+            this.motorbikes = aux
+            console.log("Filtered")
+            console.log(aux);
+
+            if (this.motorbikes.length < 1) {
+                this.noResults = true
+            }
+        }
+
+        // solucionar el guardado de las motos no va el filter
+        this.cleanFilter = function() {
+            this.filtering = false;
+            this.motorbikes = this.auxMotorbikes;
+        }
     }
 }
 
